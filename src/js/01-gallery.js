eea013784a1bec8877a24167bd9f1208667b2506
Dmitriy Gonharov <my.webpage057@gmail.com>
2023-02-11 00:28:20 +0200

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+function createGalleryItemsMarkup(items) {
+    return items.map(({preview, original, description}) => {
+       return `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      </div>`
+    }).join('');
+}
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+galleryEl.addEventListener('click', onModalOpen);
+
+function onModalOpen(evt) {
+    evt.preventDefault();
+    
+    if(evt.target.nodeName !== "IMG") {
+        return;
+    };
+
+    addSimpleLightBox();
+};
+
+function addSimpleLightBox() {
+    const lightbox = new SimpleLightbox('.gallery__item', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+        enableKeyboard: true,
+    });
+}
